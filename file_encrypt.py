b8da9c3a54c261e2fb261006a5f5eb062dcec5fe
Jamie Morrissey <jamie_morrissey@Jamies-MacBook-Air.local>
2022-12-28 14:53:56 -0800

    added main function in end

--- file_encrypt.py
+++ file_encrypt.py
@@ -1,13 +1,14 @@
 import hashlib
 import os
 import getopt, sys
+import logging
 from cryptography.fernet import Fernet, MultiFernet
 
 argumentList = sys.argv[1:]
 
 options = "hmo:"
 
-long_options = ["One", "Two", "Three", "Four", "Five"]
+long_options = ["One", "Two", "Three", "Four"]
 
 message = "some secret message".encode()
 
@@ -18,28 +19,24 @@ fernet = Fernet(key)
 
 message = "hello"
 
-def create_key(): # print string key
-    key1 = Fernet(Fernet.generate_key())
-    key2 = Fernet(Fernet.generate_key())
-    f = MultiFernet([key1, key2])
-    token = f.encrypt(b"Secret message!")
-    print(token)
 
-def decrypt_token():
-    f.decrypt(token)
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["one", "two", "three", "four="])
+        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "one", "two", "three", "four="])
 
     except getopt.GetoptError as err:
         print(err)
-        sys.exit()
+        usage()
+        sys.exit(2)
     output = None
     verbose = False
     for o, a in opts:
-        if o == "-h": # display help
+        if o == "-v":
             verbose = True
+        elif o in ("-h", "--help"): # display help
+            usage()
+            sys.exit()
         elif o in ("-md5", "--one"):
             print(compute_md5("file_encrypt.py"))
         elif o in ("-sha256", "--two"):
@@ -48,20 +45,8 @@ def main():
             create_key()
         elif o in ("-decrypt", "--four"):
             decrypt_token()
-        elif o in ("--encmessage"):
-            encMessage()
         else:
             assert False, "unhandled option"
-
-def find_files(filename, search_path):
-    result = []
-
-    for root, dirs, files in os.walk(search_path):
-        if filename in files:
-            return result.append(root, file_name)
-        return result
-
-print(find_files("README.md", "C:\\"))
 
 def compute_md5(file_name):
     hash_md5 = hashlib.md5()
@@ -73,71 +58,20 @@ def compute_md5(file_name):
 def compute_sha256(file_name):
     with open(file_name, "rb") as f:
         bytes = f.read() # read entire file as bytes
-        readable_hash = hashlib.sha256(bytes).hexdigest();
+        readable_hash = hashlib.sha256(bytes).hexdigest()
         print(readable_hash)
 
-def encMessage():
-    encMessage = fernet.encrypt(message.encode())
-    print(encMessage)
+def create_key(): # print string key
+    key1 = Fernet(Fernet.generate_key())
+    key2 = Fernet(Fernet.generate_key())
+    f = MultiFernet([key1, key2])
+    token = f.encrypt(b"Secret message!")
+
+def decrypt_token():
+    f.decrypt(token)
 
 with open('mykey.key', 'wb') as mykey:
     mykey.write(key)
 
-
-
-"""
-class encryptor():
-
-    def key_create(self):
-        key = Fernet.generate_key()
-        return key
-
-    def key_write(self, key, key_name):
-        with open(key_name, 'wb') as mykey:
-            mykey.write(key)
-
-    def key_load(self, key_name):
-        with open(key_name, 'rb') as mykey:
-            key = mykey.read()
-        return key
-
-    def encrypt(self, key):
-
-        f = Fernet(key)
-
-        with open('mykey.key', 'rb') as mykey:
-            key = mykey.read()
-        print(mykey)
-
-        with open('grades.csv', 'rb') as original_file:
-            original = original_file.read()
-
-        encrypted = f.encrypt(original)
-
-        with open ('enc_grades.csv', 'wb') as encrypted_file:
-            encrypted_file.write(encrypted)
-
-    def decrypt (self, key):
-        with open('enc_grades.csv', 'rb') as encrypted_file:
-            encrypted = encrypted_file.read()
-
-        decrypted = f.decrypt(encrypted)
-
-        with open('dec_grades.csv', 'wb') as decrypted_file:
-            decrypted_file.write(decrypted)
-
-
-encryptor=encryptor()                
-mtkey=encryptor.key_create()
-encryptor.key_write(mykey, 'mykey.key')
-load_key=encryptor.key_load('mykey.key')
-encryptor.file_encrypt(loaded_key, 'grades.csv', 'enc_grades.csv')
-encryptor.file_decrypt(loaded_key, 'enc_grades.', 'dec_grades.csv')
-
-"""
-
-
-
-
 if __name__ == "__main__":
-    create_key()
+    main()
